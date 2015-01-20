f1df637a521cb24803f2efb9519650a17c60bf5c
Justin Wilkinson <wilkinsense@gmail.com>
2015-01-20 17:31:06 -0500

    Forgot the renderer

--- CST8237-Work/GameObject.h
+++ CST8237-Work/GameObject.h
@@ -2,13 +2,15 @@
 
 #include "MathUtils.h"
 
+struct SDL_Renderer;
+
 class GameObject
 {
 public:
   virtual void Initialize() = 0;
   
   virtual void Update(float dt) = 0;
-  virtual void Draw(float dt) = 0;
+  virtual void Draw(SDL_Renderer *renderer, float dt) = 0;
 
   ~GameObject();
 
